4ef80ce57caaf8ce0a103c9db9d2e40a7f5e52ac
Gonzalo <gepinillab@gmail.com>
2019-05-04 07:34:48 -0500

    created doAddRem files

---
+++ R/mask_doAddRem.R
@@ -0,0 +1,29 @@
+
+#' @title mask_doAddRem
+#' @description ..
+#'
+#' @details
+#' See Examples.
+#'
+#' @param shinyLogs x
+# @keywords
+#'
+# @examples
+#'
+#'
+# @return
+#' @author Gonzalo Pinilla gpinillabuitrago@@gradcenter.cuny.edu
+# @note
+# @seealso
+# @references
+# @aliases - a list of additional topic names that will be mapped to
+# this documentation when the user looks them up from the command
+# line.
+# @family - a family name. All functions that have the same family tag will be linked in the documentation.
+
+#' @export
+#'
+
+mask_doAddRem <- function(shinyLogs = NULL) {
+
+}
---
+++ inst/shiny/modules/mod_mask_doAddRem.R
@@ -0,0 +1,16 @@
+doAddRem_UI <- function(id) {
+  ns <- NS(id)
+  tagList(
+
+  )
+}
+
+doAddRem_MOD <- function(input, output, session) {
+  reactive({
+
+  })
+}
+
+doAddRem_MAP <- function(map, session) {
+
+}
